b7a1db29651f1bae28086aea70e9d3a149583421
blue55 <yllan@fiberhome.com>
2017-06-19 14:04:45 +0800

    Fix html_last_updated_fmt for Python3
    Fix the Sphinx html_last_updated_fmt for Python3. The
    html_last_updated_fmt option is interpreted as a
    byte string in python3, causing Sphinx build to break.
    This patch makes it utf-8 string.
    
    Change-Id: I117e4be75ca77cfc601225bc70c518bf4fb6f071

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,9 +12,10 @@
 # serve to show the default.
 
 import datetime
-import os
 import subprocess
 import sys
+import os
+import warnings
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -137,9 +138,11 @@ html_theme = 'nature'
 # using the given strftime format.
 git_cmd = ["git", "log", "--pretty=format:'%ad, commit %h'", "--date=local -",
    "n1"]
-html_last_updated_fmt = subprocess.Popen(git_cmd,
-                                         stdout=subprocess.PIPE).\
-                                         communicate()[0]
+try:
+    html_last_updated_fmt = subprocess.check_output(git_cmd).decode('utf-8')
+except Exception:
+    warnings.warn('Cannot get last updated time from git repository. '
+                  'Not setting "html_last_updated_fmt".')
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
